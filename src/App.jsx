43b431237ee44a3a6ed4e13829dd6bac7dfdd336
Giuliana <giuliana9566@gmail.com>
2024-03-02 17:03:47 -0300

    Agregamos la maquetacion de la lista de noticias

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,21 @@ function App() {
     <>
       <Navbar />
       <MainInformation />
+      <div>
+        <h2>New </h2>
+        <div>
+          <h3>Hydrogen VS Electric Cars</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+        </div>
+        <div>
+          <h3>Hydrogen VS Electric Cars</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+        </div>
+        <div>
+          <h3>Hydrogen VS Electric Cars</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+        </div>
+      </div>
     </>
   );
 }
