26f77bdc332c0690e2d5d81adfbc5b900c14c374
Agustin Vera <agustin.vera@possumus.tech>
2024-03-02 18:05:54 -0300

    add: Navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,33 @@
-import logo from '../../assets/images/logo.svg'
+import "../styles/Navbar.css";
+import logo from "../../assets/images/logo.svg";
 
-export default function Navbar(){
-    return <nav>
-        <ul>
-            <li><a href="/"><img src={logo} alt="" /></a></li>
-            <li><a href="/">Home</a></li>
-            <li><a href="/new">New</a></li>
-            <li><a href="#">Popular</a></li>
-            <li><a href="#">Trending</a></li>
-            <li><a href="#">Categories</a></li>
-        </ul>
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">
+            <img src={logo} alt="" />
+          </a>
+        </li>
+        <div className="contenedorEnlaces">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/new">New</a>
+          </li>
+          <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+        </div>
+      </ul>
     </nav>
+  );
 }
