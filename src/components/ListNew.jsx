a62928dbc6bf4f4c7a367dd3f808248298329f74
Giuliana <giuliana9566@gmail.com>
2024-03-06 02:43:42 -0300

    Add: ListNew de css, comenzando con styles

--- src/components/ListNew.jsx
+++ src/components/ListNew.jsx
@@ -1,17 +1,20 @@
+import "../styles/ListNew.css";
 export default function ListNew(){
     return <div>
-    <h2>New </h2>
+    <h2 id= "Titulo">New </h2>
     <div>
-      <h3>Hydrogen VS Electric Cars</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+      <h3 id= "Subtitulo">Hydrogen VS Electric Cars</h3>
+      <p  id="MiniParrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+      <hr></hr>
     </div>
     <div>
-      <h3>The Dowsides of AI Artistry</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+      <h3 id= "Subtitulo">The Dowsides of AI Artistry</h3>
+      <p  id="MiniParrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+      <hr></hr>
     </div>
     <div>
-      <h3>Is VC Funding Drying Up?</h3>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
+      <h3 id= "Subtitulo">Is VC Funding Drying Up?</h3>
+      <p  id="MiniParrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. At in perferendis placeat itaque! Veritatis consequatur natus earum?</p>
     </div>
   </div>
 }
